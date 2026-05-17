f6899e121fc4f0ad0a66f97933165a8dc9f12d60
Nick An <nickooan@gmail.com>
2026-05-17 12:24:43 +1000

    update version to v0.2.0

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { render } from "ink"
 import { execute, resolveRoot } from "./src/runtime/command.ts"
 import { TerminalApp } from "./src/views/terminal-app.tsx"
 
-export const VERSION = "0.1.1"
+export const VERSION = "0.2.0"
 
 const CommandApp = ({ args }: { args: string[] }) => {
   const root = useMemo(() => resolveRoot(args), [args])
